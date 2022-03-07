386a02b88de9f9ae04903b7b0f233371609b776d
JLBegin <ludovick.begin@gmail.com>
2022-03-07 11:12:58 -0500

    Update tournesol.py

--- tournesol.py
+++ tournesol.py
@@ -9,9 +9,3 @@ class Tournesol:
     def grow(self, sunlight: float, resources: float):
         # Write code here
         raise NotImplementedError()
-
-    def _growBranch(self):
-        self.branches += 1
-
-    def _growHigher(self):
-        self.height += random.gauss(0.01 + self.branches * 0.002, 0.002)
